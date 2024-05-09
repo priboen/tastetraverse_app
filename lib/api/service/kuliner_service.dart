a2ea327f12779dadf68becf2663ed7047df3123a
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 04:27:51 +0700

    membuat method untuk mendapatkan data kuliner dari API

--- lib/api/service/kuliner_service.dart
+++ lib/api/service/kuliner_service.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class KulinerService {
   // final String baseUrl = "http://10.0.2.2:8000/api/";
   final String baseUrl = "http://127.0.0.1:8000/api/";
@@ -6,5 +10,22 @@ class KulinerService {
 
   Uri getUri(String path) {
     return Uri.parse("$baseUrl$path");
+  }
+
+  Future<List<dynamic>> getKuliner() async {
+    var response = await http.get(
+        getUri(
+          endpoint,
+        ),
+        headers: {
+          "Accept": "application/json",
+        });
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedResponse = json.decode(response.body);
+      return decodedResponse['data'];
+    } else {
+      throw Exception('Tidak dapat mengambil data: ${response.reasonPhrase}');
+    }
   }
 }
