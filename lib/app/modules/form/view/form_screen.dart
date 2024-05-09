e9b4fa7577a74da0cfcedd9fb38cbcaf4a0e2513
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 03:59:03 +0700

    membuat form screen

---
+++ lib/app/modules/form/view/form_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Masukan Data Kuliner"),
+      ),
+      body: Form(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
