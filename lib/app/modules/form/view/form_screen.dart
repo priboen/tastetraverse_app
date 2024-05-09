d51cd533c7bdc08bc88c723518564d4eee7fb7a2
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 04:14:49 +0700

    menambahkan widget text field

--- lib/app/modules/form/view/form_screen.dart
+++ lib/app/modules/form/view/form_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tastetraverse_app/app/modules/form/widget/form_header.dart';
+import 'package:tastetraverse_app/app/modules/form/widget/text_field.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
@@ -20,11 +21,40 @@ class _FormScreenState extends State<FormScreen> {
         title: const Text("Masukan Data Kuliner"),
       ),
       body: Form(
+          key: _formKey,
           child: SingleChildScrollView(
-        child: Column(
-          children: [FormHeaderWidget()],
-        ),
-      )),
+            child: Column(
+              children: [
+                FormHeaderWidget(),
+                TextFieldWidget(
+                  textcontroller: _namaController,
+                  labelParam: 'Nama Kuliner',
+                  hintParam: 'Masukan Nama Kuliner',
+                  iconParam: Icon(
+                    Icons.food_bank,
+                  ),
+                  inputType: TextInputType.text,
+                  messageParam: 'Nama Kuliner tidak boleh kosong!',
+                ),
+                TextFieldWidget(
+                  textcontroller: _jenisController,
+                  labelParam: 'Jenis Kuliner',
+                  hintParam: 'Masukan Jenis Kuliner',
+                  iconParam: Icon(Icons.fastfood),
+                  inputType: TextInputType.text,
+                  messageParam: 'Jenis Kuliner tidak boleh kosong!',
+                ),
+                TextFieldWidget(
+                  textcontroller: _hargaController,
+                  labelParam: 'Harga Kuliner',
+                  hintParam: 'Masukan Harga Kuliner',
+                  iconParam: Icon(Icons.price_change),
+                  inputType: TextInputType.number,
+                  messageParam: 'Harga tidak boleh kosong!',
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
