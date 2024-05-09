7de362250242f09c3a39f0f3e9da02988c0f7ab6
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 04:38:01 +0700

    menampilkan data dari API

--- lib/app/modules/home/view/home_screen.dart
+++ lib/app/modules/home/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tastetraverse_app/api/controller/kuliner_controller.dart';
+import 'package:tastetraverse_app/app/data/kuliner.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,8 +15,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TasteTraverse'),
+        title: Text(
+          'TasteTraverse',
+        ),
       ),
+      body: FutureBuilder<List<Kuliner>>(
+          future: _kulinerController.getKuliner(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].nama),
+                    subtitle: Text(snapshot.data![index].jenis),
+                    trailing: Text(snapshot.data![index].harga.toString()),
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
