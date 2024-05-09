08a9eef6de10f083f581fac37aa1e72e5aedb518
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 04:33:58 +0700

    deklarasi Kuliner Controller

--- lib/app/modules/home/view/home_screen.dart
+++ lib/app/modules/home/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tastetraverse_app/api/controller/kuliner_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final KulinerController _kulinerController = KulinerController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
