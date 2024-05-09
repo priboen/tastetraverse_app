84e8dc85c76498d2a049b618841b9722699f0971
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-05-10 04:45:55 +0700

    menambahkan floating action button dan route ke FormScreen

--- lib/app/modules/home/view/home_screen.dart
+++ lib/app/modules/home/view/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tastetraverse_app/api/controller/kuliner_controller.dart';
 import 'package:tastetraverse_app/app/data/kuliner.dart';
+import 'package:tastetraverse_app/app/modules/form/view/form_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,29 +21,41 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: FutureBuilder<List<Kuliner>>(
-          future: _kulinerController.getKuliner(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data![index].nama),
-                    subtitle: Text(snapshot.data![index].jenis),
-                    trailing: Text(snapshot.data![index].harga.toString()),
-                  );
-                },
-              );
-            }
-          }),
+        future: _kulinerController.getKuliner(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].nama),
+                  subtitle: Text(snapshot.data![index].jenis),
+                  trailing: Text(snapshot.data![index].harga.toString()),
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const FormScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
